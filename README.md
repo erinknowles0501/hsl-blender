2e9690ae9850aed1574d685198bf2e2e9b99f1cd
Erin <colorandcontrast@gmail.com>
2021-09-06 00:37:27 -0300

    Bugfixes, cleanup, sanity changes. Update README to include some missed info.

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # HSL (hue-saturation-lightness) blender
 
-_Pure JS (ES6). Long term plan - separate the logic out into a module that I can pull into the project I originally wrote this for._
-
 ## Overview
 
-The color format HSL has values that range from 0-360, with red at both ends. We can therefore think of it as a circle.
+HSL (color format: Hue Saturation Lightness) values start at red, at 0, pass through the rainbow, and end up at red again at 360. This makes accurately blending (ie getting the color halfway between two given colors) counter-intuitive - the classic way to average a number (`(x+y)/2`) doesn’t always work here (take the example of hue `30` (orange) and hue `330` (magenta) - they should average to red (`0` or `360`), but instead, `(30+330)/2 = 180`, which is cyan!) And so, you need to think of the hues as a circle, convert the polar coordinates to cartesian coordinates, get the average of the hypotenuse, and convert the average back into polar.
 
-This experiment/tool was born because the classic way to average a number (`(x+y)/2`) doesn’t always work here (take the example of hue `30` (orange) and hue `330` (magenta) - they should average to red (`0` or `360`, but instead, `(30+330)/2 = 180`, which is cyan!).
+Because the HSL blender started as a codepen experiment, I decided to lean in to how to organize the code so it would make sense and be readable all in one file, instead of using folder structure like I usually do.
 
-And so, to get the average of these two colors, we convert their polar coordinates (just a degree, really, because we can assume radius=1) to cartesian coordinates, average the hypotenuse, and then convert that point back into a polar coordinate.
+Written in pure JS (es6);
 
 ## File structure
 
 TODOs at top. Then define a global variable (currentAverageHue) and some DOM elements as consts.
-Then, define the classes - DOMConnector, Hue, and Error.
+Then, define the classes - DOMConnector, Hue (which is inherited by HuePolar and HueCartesian), and Error.
 Them the DOMConnector is initialized, it does its first run (to set values based on defaults). We set some event listeners (input change, button click).
 Then there's one little function that actually does the blending of the colors.
 
@@ -21,11 +19,12 @@ Then there's one little function that actually does the blending of the colors.
 
 This was originally intended as a quick proof-of-concept for implementing the polar->cartesian->polar method in another project, but when I finished, I found myself wanting to refactor and improve it, and it became its own mini-project. You can see the list of TODO's in comments near the top of the main file.
 
-Usually these fiddles/pens are just to check if something definitely works in isolation before trying to introduce it into a project (and sometimes the reverse - something isn't working, let's see if I can make it work by itself).
+Usually these fiddles/pens are just to check if something definitely works in isolation before trying to introduce it into a project (and sometimes the reverse - something isn't working, let's see if I can make it work by itself). Sometimes, I suppose, they turn into their own project - I now want to both build out this tool, and isolate the blending so I can export it as a module and use it out-of-the-box in the project this was originally meant for.
 
-I thought a lot about how to make it accessible for the 'next' dev, if there is one. I used descriptive (longer) var, prop, and function names - eg, 'radius' instead of 'r', or 'getRadiansFromDegrees(degrees)' instead of 'getRfromD(d)', because it removes the 'translation' step required for comprehension.
-I also got serious about reminding the reader what's going into and out of a function. I made (mock) docblocks with what the function does, what values it expects, and what values it outputs, along with a reminder of the content or structure of any arrays or objects.
+For fun, I ended up thinking a lot about how to make it accessible for the 'next' dev. I noticed I used descriptive (longer) var, prop, and function names - eg, 'radius' instead of 'r', or 'getRadiansFromDegrees(degrees)' instead of 'getRfromD(d)', because it removes the 'translation' step required for comprehension.
+I also got serious about reminding the reader what's going into and out of a function. I made (mock...it's JS) docblocks with what the function does, what values it expects, and what values it outputs, along with a reminder of the content or structure of any arrays or objects.
 I moved the business logic into classes to separate them (and allow collapsing..!).
-I also added an Error class - it just spits out a descriptive, color-coded console error when something I anticipate goes wrong - but it has already saved me time!
+I also added an Error class as a proof-of-concept - it just spits out a descriptive, color-coded console error when something I anticipate goes wrong - but it has actually saved me time!
 
-- Talk about your back-and-forth about how to set Hue coordinates - either use a factory, or...?
+I spent a lot of time thinking about how to set up a Hue, where it's defined as either a polar or a cartesian hue, and hues usually have to convert back and forth. My first approaches involved passing a `type` to the constructor that was `switch`ed on whether or not it matched the constant value for cartesian or polar, but I really hate switches.
+Eventually I realized I'm only intending to work with two discrete types of hues with a lot of functionality and data structure in common, and that a new Hue, given the scope of what I have planned for this tool, will always require being defined with a type, so now, classes HuePolar and HueCartesian inherit from Hue.
--- blender.js
+++ blender.js
@@ -1,16 +1,9 @@
 // In rough order of importance:
-// TODO: Refactor:
-//       - Add Hue class with methods
-//       - Some functions could use more/less complexity.
-//       - Cleverer or more readable versions of some functions.
-//       - Ensure hue0 and hue1 everywhere - not color, not 1+2.
 // TODO: Add polar+cartesian charts with points.
 // TODO: Some sanity styling for intuitive use. (also add bg to text on color for easier reading)
-// TODO: Add docblocks to functions/methods
 // TODO: Validation on set() coordinates.
-// TODO: Build polar-based color selector.
-// TODO: Add support for multiple hues to be blended
-// TODO: Fix null/null bug
+// TODO: Build polar-based color selector!
+// TODO: Add support for multiple hues to be blended?
 
 // Set up global variables:
 let currentAverageHue = 0;
@@ -20,7 +13,7 @@ const hue0Element = document.getElementById("hue0");
 const hue1Element = document.getElementById("hue1");
 const appBackgroundElement = document.getElementById("app-background");
 
-// Define classes: DOMConnector, Hue, Error
+// Define classes: DOMConnector, Hue (Inheritable), HuePolar, HueCartesian, Error
 class DOMConnector {
   /**
    * Creates CSS hsl (hue, saturation, lightness) string from hue, with default saturation and lightness.
@@ -31,8 +24,11 @@ class DOMConnector {
   hslFromHue(hue) {
     // Hue=0 is fine, everything else is not. Put the Hue=0 check first
     // because it's more likely than the other items.
-    if (hue !== 0 && (!hue || isNaN(hue) || hue < 0 || hue > 360)) {
-      new Error(Error.DEV_MESSAGE_TYPES.ERROR, `Hue ${hue} does not exist.`);
+    if (hue !== 0 && (!hue || isNaN(hue))) {
+      new Message(
+        Message.DEV_MESSAGE_TYPES.ERROR,
+        `Hue ${hue} does not exist.`
+      );
       return;
     }
     return `hsl(${hue}, 100%, 50%)`;
@@ -42,19 +38,18 @@ class DOMConnector {
    * Call to update backgrounds of passed input elements, and update background color
    * Error if elements is present but not an array.
    *
-   * elements = array of DOM elements
+   * elements = array of DOM elements. Nullable.
    * */
   updateDisplay(elements = [hue0Element, hue1Element]) {
     if (elements && !Array.isArray(elements)) {
-      new Error(
-        Error.DEV_MESSAGE_TYPES.ERROR,
+      new Message(
+        Message.DEV_MESSAGE_TYPES.ERROR,
         `Elements should be an array, value ${elements} is not an array.`
       );
       return;
     }
 
-    appBackgroundElement.style.backgroundColor =
-      this.hslFromHue(currentAverageHue);
+    this.updateAppBackground();
 
     // No elements need to be updated - return out of this function before the forEach runs.
     if (!elements) {
@@ -64,6 +59,19 @@ class DOMConnector {
     elements.forEach((element) => {
       this.updateInputBackground(element);
     });
+
+    // Since there is at least one element, that means at least one color has changed,
+    // which means we need to re-blend our colors.
+    getBlendedColor();
+    this.updateAppBackground();
+  }
+
+  /**
+   * Update the page background to current version of currentAverageHue.
+   * */
+  updateAppBackground() {
+    appBackgroundElement.style.backgroundColor =
+      this.hslFromHue(currentAverageHue);
   }
 
   /**
@@ -78,93 +86,91 @@ class DOMConnector {
 
 /**
  * Handles and stores hue information and logic.
- * Error if invalid type.
- *
- * coords: One of polar or cartesian coordinates object (see BLANK_* constants)
- * type: One of 'polar' or 'cartesian'. See COORD_SYSTEM_TYPES constant above
+ * Not directly used except for lookup of static props - inherited by HuePolar and HueCartesian.
  * */
 class Hue {
-  // TODO: Consider: Hue is just an inheritance, and actual implementation uses HuePolar or HueCartesian?
-  // Before you make that call, quickly spec out the TODO's graph views. Will using a factory be a pain in the ass there?
-
   static COORD_SYSTEM_TYPES = { POLAR: "polar", CARTESIAN: "cartesian" };
   static BLANK_CARTESIAN_COORDS = { x: 0, y: 0 };
   static BLANK_POLAR_COORDS = { theta: 0 }; // don't need radius here.
 
-  polar = {};
-  cartesian = {};
-
-  constructor(coords, type) {
-    if (!Object.values(Hue.COORD_SYSTEM_TYPES).includes(type)) {
-      new Error(
-        Error.DEV_MESSAGE_TYPES.ERROR,
-        `Coordinate system ${type} is not supported or does not exist.`
-      );
-    }
-
-    // (JSON parse/stringify is safest/fastest/easiest/smallest overall method to deep clone an object.)
-    this.polar = JSON.parse(JSON.stringify(Hue.BLANK_POLAR_COORDS)); // polar: { radius, theta }
-    this.cartesian = JSON.parse(JSON.stringify(Hue.BLANK_CARTESIAN_COORDS)); // cartesian: { x, y }
-
-    // Does this work?
-    console.log(this, coords);
-    this[type] = coords;
-
-    // then set the other, whatever isn't [type]
-    // this.setPolarCoords(coords);
-    // this.setCartesianCoords(coords);
-  }
+  // (JSON parse/stringify is safest/fastest/easiest/smallest overall method to deep clone an object.)
+  polar = JSON.parse(JSON.stringify(Hue.BLANK_POLAR_COORDS)); // { theta }
+  cartesian = JSON.parse(JSON.stringify(Hue.BLANK_CARTESIAN_COORDS)); // { x, y }
 
   /**
-   * Error on missing or invalid coords
+   * Error on missing or invalid value
    *
-   * coords: either polar or cartesian.
+   * theta: Number: since we don't need the radius, we're just using the degree.
    * */
-  setCartesianCoordsFromPolar(coords) {
-    if (!coords || !coords.radius || !coords.theta) {
-      new Error(
-        Error.DEV_MESSAGE_TYPES.ERROR,
-        `Coordinates ${coords} can not be converted to cartesian coords.`
+  setCartesianCoordsFromPolar(theta) {
+    if (theta !== 0 && !theta) {
+      new Message(
+        Message.DEV_MESSAGE_TYPES.ERROR,
+        `Theta is missing, cannot convert to cartesian coordinates.`
       );
       return;
     }
 
     // Math.cos() and .sin() expect radians. Convert the hue degree to radians first,
     // then convert it back to degrees for the cartesian coords.
-    const hueRadians = this.getRadiansFromDegrees(this.polar.theta);
-    const hueCartesian = {};
-    hueCartesian.x = this.getDegreesFromRadians(Math.cos(hueRadians));
-    hueCartesian.y = this.getDegreesFromRadians(
-      Math.sin(hueRadians) * (180 / Math.PI)
-    );
-    return hueCartesian;
+    const hueRadians = this.getRadiansFromDegrees(theta);
+
+    this.cartesian.x = this.getDegreesFromRadians(Math.cos(hueRadians));
+    this.cartesian.y = this.getDegreesFromRadians(Math.sin(hueRadians));
   }
 
   /**
    * Error on missing or invalid coords
    *
-   * coords: either polar or cartesian.
+   * coords: polar. No radius, so just { theta }
    * */
   setPolarCoordsfromCartesian(coords) {
-    let hueRadians = Math.atan2(this.cartesian.y, cartesian.x);
-    let hueDegrees = hueRadians * (180 / Math.PI);
+    if (!coords || !coords.x || !coords.y) {
+      new Message(
+        Message.DEV_MESSAGE_TYPES.ERROR,
+        `Coordinates ${coords} can not be converted to polar coords.`
+      );
+      return;
+    }
+
+    let hueRadians = Math.atan2(coords.y, coords.x);
+    let hueDegrees = this.getDegreesFromRadians(hueRadians);
 
     // Special case if both are negative: means we're in quadrant 3,
     // where the degree has to be flipped from quad 1 to quad 3.
-    if (cartesian.x < 0 && cartesian.y < 0) {
+    if (coords.x < 0 && coords.y < 0) {
       hueRadians = 2 * Math.PI + hueRadians;
-      hueDegrees = hueRadians * (180 / Math.PI);
+      hueDegrees = this.getDegreesFromRadians(hueRadians);
     }
 
     this.polar.theta = hueDegrees;
   }
 
+  // These two could be "convertBetweenDegreesAndRadians(value, direction)" but let's not.
   getDegreesFromRadians(radians) {
     return radians * (180 / Math.PI);
   }
 
   getRadiansFromDegrees(degrees) {
     return degrees * (Math.PI / 180);
+  }
+}
+
+class HuePolar extends Hue {
+  constructor(theta) {
+    super();
+    this.polar.theta = theta;
+
+    this.setCartesianCoordsFromPolar(theta);
+  }
+}
+
+class HueCartesian extends Hue {
+  constructor(coords) {
+    super();
+    this.cartesian = coords;
+
+    this.setPolarCoordsfromCartesian(coords);
   }
 }
 
@@ -175,7 +181,7 @@ class Hue {
  *       and a string color property (to give color context to the message).
  * message: string.
  * */
-class Error {
+class Message {
   static DEV_MESSAGE_TYPES = {
     ERROR: {
       label: "Error",
@@ -207,30 +213,33 @@ hue0Element.addEventListener("change", () => {
 hue1Element.addEventListener("change", () => {
   domConnector.updateDisplay([hue1Element]);
 });
-document.getElementById("blend").addEventListener("click", getBlendedColor);
+document.getElementById("blend").addEventListener("click", getBlendedColor());
 
-// The business!
-function getBlendedColor(hue0 = null, hue1 = null) {
-  hue0 = new Hue(
-    !!hue0 ? hue0 : Number(hue0Element.value),
-    Hue.COORD_SYSTEM_TYPES.POLAR
-  );
-  hue1 = new Hue(
-    !!hue1 ? hue1 : Number(hue1Element.value),
-    Hue.COORD_SYSTEM_TYPES.POLAR
-  );
+/**
+ * The business part!
+ * Error if hue missing or invalid.
+ * */
+function getBlendedColor() {
+  let hue0 = Number(hue0Element.value);
+  let hue1 = Number(hue1Element.value);
 
-  // Get the hue's cartesian values and average into a new cartesian.
-  const averageHueCartesian = new Hue(
-    {
-      x: (hue0.cartesian.x + hue1.cartesian.x) / 2,
-      y: (hue0.cartesian.y + hue1.cartesian.y) / 2,
-    },
-    Hue.COORD_SYSTEM_TYPES.CARTESIAN
-  );
+  if ((hue0 !== 0 && !hue0) || (hue1 !== 0 && !hue1)) {
+    new Message(
+      Message.DEV_MESSAGE_TYPES.ERROR,
+      `Cannot blend colors: Invalid or missing hue(s): ${hue0}, ${hue1}`
+    );
+    return;
+  }
+
+  hue0 = new HuePolar(hue0);
+  hue1 = new HuePolar(hue1);
+
+  // Get the hue's cartesian values and average into a new cartesian
+  const averageHueCartesian = new HueCartesian({
+    x: (hue0.cartesian.x + hue1.cartesian.x) / 2,
+    y: (hue0.cartesian.y + hue1.cartesian.y) / 2,
+  });
 
   // Convert back to polar and save to global.
   currentAverageHue = averageHueCartesian.polar.theta;
-
-  domConnector.updateDisplay();
 }
